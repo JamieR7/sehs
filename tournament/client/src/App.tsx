ff39c0c612c45d3bf3046de8c8662a39264c1a39
JamieR <thatjamiebloke@gmail.com>
2026-09-15 08:05:30 +0200

    Add files via upload

---
+++ tournament/client/src/App.tsx
@@ -0,0 +1,31 @@
+import { Switch, Route, Router } from "wouter";
+import { queryClient } from "./lib/queryClient";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { Toaster } from "@/components/ui/toaster";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import NotFound from "@/pages/not-found";
+import TournamentPage from "@/pages/tournament";
+
+function AppRouter() {
+  return (
+    <Router base="/tournament">
+      <Switch>
+        <Route path="/" component={TournamentPage} />
+        <Route component={NotFound} />
+      </Switch>
+    </Router>
+  );
+}
+
+function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <TooltipProvider>
+        <Toaster />
+        <AppRouter />
+      </TooltipProvider>
+    </QueryClientProvider>
+  );
+}
+
+export default App;
